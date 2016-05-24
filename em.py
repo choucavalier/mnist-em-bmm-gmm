31e3337eea61f5c0278d52f0649c01fbd2fac143
toogy <toogy@too.gy>
2016-05-24 20:40:43 +0200

    (bmm) now using numpy

--- em.py
+++ em.py
@@ -39,53 +39,42 @@ class bmm_em(em):
         super().__init__(k, x, iterations)
         self.d = d
 
-        self.mu = np.random.rand(self.k, self.d)
+        self.mu = np.ndarray(shape=(self.k, self.d))
 
+        # initialization of mu
         for m in range(self.k):
-
-            normalization_factor = 0.0
             for i in range(self.d):
                 self.mu[m,i] = np.random.random() * 0.5 + 0.25
-                normalization_factor += self.mu[m, i]
-            for i in range(self.d):
-                self.mu[m,i] /= normalization_factor
+
+        print(self.mu)
 
     def expectation_step(self):
 
-        prod = np.zeros(self.k)
+        logsum = np.ndarray(shape=(self.k, self.n))
 
-        for n in range(self.n):
-            for m in range(self.k):
-                t = self.pi[m]
-                t *= np.prod(np.power(self.mu[m], self.x[n]))
-                t *= np.prod(np.power((1.0 - self.mu[m]), (1.0 - self.x[n])))
-                prod[m] = t
+        for k in range(self.k):
+            logsum[k, ...] = np.log(self.pi[k]) \
+                + np.log(np.prod(self.mu[k, ...] ** self.x, 1)) \
+                + np.log(np.prod((1 - self.mu[k, ...]) ** (1 - self.x), 1))
 
-        s = sum(prod)
-        for n in range(self.n):
-            for m in range(self.k):
-                if s > 0.0:
-                    self.z[m,n] = prod[m] / s
-                else:
-                    self.z[m,n] = prod[m] / float(self.k)
+        prod = np.exp(logsum)
+
+        for k in range(self.k):
+            self.z[k] = prod[k] / np.sum(prod[k])
+
+        print('z', self.z)
 
     def maximization_step(self):
 
-        for m in range(self.k):
-            n_m = np.sum(self.z[m])
-            self.pi[m] = n_m / self.n # update pi
-            self.mu[m] = 0
-            for i in range(self.n):
-                self.mu[m] += self.z[m,i] * self.x[i].T
-            self.mu[m] /= n_m
+        n_ms = np.sum(self.z, 1)
+        # updating mu
+        mu = np.dot(np.diag(1 / n_ms), np.dot(self.z, self.x))
+        # updating pi
+        self.pi = n_ms / self.n
 
     @property
     def llk(self):
 
-        llk = 0
-        for n in range(self.n):
-            for k in range(self.k):
-                s = np.sum(self.x[n] * np.log(self.mu[k]))
-                s += np.sum((1.0 - self.x[n]) * np.log(1 - self.mu[k]))
-                llk += self.z[k,n] * (np.log(self.pi[k]) + s)
-        return llk
+        # TODO
+
+        return 0
